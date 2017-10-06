cb1eada1e0a1fab5b59447fd0b0414585f123cbd
Jacek Gałka <jacgalka@icloud.com>
2017-10-06 17:55:54 +0200

    Additional view controllers

---
+++ RainbowTabs/YellowViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YellowViewController.swift
+//  RainbowTabs
+//
+//  Created by Jacek Gałka on 06.10.2017.
+//  Copyright © 2017 Jacek Gałka. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
